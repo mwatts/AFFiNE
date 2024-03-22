fdc43b8ec99ca471a071bc7f68840472e8175205
JimmFly <447268514@qq.com>
2024-03-22 13:55:09 +0800

    feat(core): add remove from collection to collection page list

--- packages/frontend/core/src/components/page-list/operation-cell.tsx
+++ packages/frontend/core/src/components/page-list/operation-cell.tsx
@@ -18,6 +18,7 @@ import {
   FavoritedIcon,
   FavoriteIcon,
   FilterIcon,
+  FilterMinusIcon,
   MoreVerticalIcon,
   OpenInNewIcon,
   ResetIcon,
@@ -43,26 +44,31 @@ export interface PageOperationCellProps {
   favorite: boolean;
   isPublic: boolean;
   link: string;
+  isInAllowList?: boolean;
   onToggleFavoritePage: () => void;
   onRemoveToTrash: () => void;
   onDuplicate: () => void;
   onDisablePublicSharing: () => void;
   onOpenInSplitView: () => void;
+  onRemoveFromAllowList?: () => void;
 }
 
 export const PageOperationCell = ({
   favorite,
   isPublic,
+  isInAllowList,
   link,
   onToggleFavoritePage,
   onRemoveToTrash,
   onDuplicate,
   onDisablePublicSharing,
   onOpenInSplitView,
+  onRemoveFromAllowList,
 }: PageOperationCellProps) => {
   const t = useAFFiNEI18N();
   const { appSettings } = useAppSettingHelper();
   const [openDisableShared, setOpenDisableShared] = useState(false);
+
   const OperationMenu = (
     <>
       {isPublic && (
@@ -72,6 +78,18 @@ export const PageOperationCell = ({
             setOpenDisableShared(true);
           }}
         />
+      )}
+      {isInAllowList && (
+        <MenuItem
+          onClick={onRemoveFromAllowList}
+          preFix={
+            <MenuIcon>
+              <FilterMinusIcon />
+            </MenuIcon>
+          }
+        >
+          {t['Remove special filter']()}
+        </MenuItem>
       )}
       <MenuItem
         onClick={onToggleFavoritePage}
