fa4ca8cfcedc9786932a4f5d21d424b6fddfa0c5
Cats Juice <wp1298554944@hotmail.com>
2024-03-23 22:37:55 +0800

    feat(core): compatible with multiple subscriptions

--- packages/frontend/core/src/hooks/use-subscription.ts
+++ packages/frontend/core/src/hooks/use-subscription.ts
@@ -1,19 +1,25 @@
 import { useAsyncCallback } from '@affine/core/hooks/affine-async-hooks';
-import { type SubscriptionQuery, subscriptionQuery } from '@affine/graphql';
+import {
+  SubscriptionPlan,
+  type SubscriptionQuery,
+  subscriptionQuery,
+} from '@affine/graphql';
 
 import { useServerFeatures } from './affine/use-server-config';
 import { useQuery } from './use-query';
 
 export type Subscription = NonNullable<
-  NonNullable<SubscriptionQuery['currentUser']>['subscription']
+  NonNullable<SubscriptionQuery['currentUser']>['subscriptions'][number]
 >;
 
 export type SubscriptionMutator = (update?: Partial<Subscription>) => void;
 
-const selector = (data: SubscriptionQuery) =>
-  data.currentUser?.subscription ?? null;
+const selector = (data: SubscriptionQuery, plan: SubscriptionPlan) =>
+  (data.currentUser?.subscriptions ?? []).find(p => p.plan === plan);
 
-export const useUserSubscription = () => {
+export const useUserSubscription = (
+  plan: SubscriptionPlan = SubscriptionPlan.Pro
+) => {
   const { payment: hasPaymentFeature } = useServerFeatures();
   const { data, mutate } = useQuery(
     hasPaymentFeature ? { query: subscriptionQuery } : undefined
@@ -22,26 +28,33 @@ export const useUserSubscription = () => {
   const set: SubscriptionMutator = useAsyncCallback(
     async (update?: Partial<Subscription>) => {
       await mutate(prev => {
-        if (!update || !prev?.currentUser?.subscription) {
+        if (!update || !prev?.currentUser?.subscriptions) {
           return;
         }
 
+        let hit = false;
         return {
           currentUser: {
-            subscription: {
-              ...prev.currentUser?.subscription,
-              ...update,
-            },
+            subscriptions: (prev.currentUser?.subscriptions ?? []).map(sub => {
+              if (!hit || sub.plan !== plan) {
+                return sub;
+              }
+              hit = true;
+              return {
+                ...sub,
+                ...update,
+              };
+            }),
           },
         };
       });
     },
-    [mutate]
+    [mutate, plan]
   );
 
   if (!hasPaymentFeature) {
     return [null, () => {}] as const;
   }
 
-  return [selector(data), set] as const;
+  return [selector(data, plan), set] as const;
 };
